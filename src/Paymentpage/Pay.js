3bd54a3e55e1cefd872d1d3b05440639b5702e72
iamshiryu <steambank12@gmail.com>
2025-04-02 06:36:05 +0700

    Add files via upload

---
+++ src/Paymentpage/Pay.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
+// import setpriceorder from '../pageorder/Logic';
+const generatePayload = require('promptpay-qr');
+
+
+export default function Pay() {
+
+  const [ phoneNumber, setPhoneNumber ] = useState("0955672080");
+  const [ amount, setAmount ] = useState();         
+  const [ qrCode ,setqrCode ] = useState("sample");
+
+  function handlePhoneNumber() {
+    setPhoneNumber("0955672080");
+  }
+  function handleAmount() {
+    setAmount(parseFloat(10));
+  }
+  function handleQR() {
+    setqrCode(generatePayload(phoneNumber, { amount }));
+  }
+  return(
+    <div>
+      <h2>I'm out of money so please donate me!</h2>
+      <input type="text" value={phoneNumber} onChange={handlePhoneNumber} />
+      <input type="number" value={amount} onChange={handleAmount} />
+      <button onClick={handleQR}>Generate Promptpay QR</button>
+      <QRCode value={qrCode} />
+    </div>
+   );
+}
+// const orderId ='E001'
+//   const amount = calprice()
+//   const qrCodeData = 'example_qr_code_data'
+//   const paymentSubmit = (event) => {
+//     event.preventDefault();
+//     const paymentInfo = { amount: amount, orderId: orderId };
+//     const body = new URLSearchParams({
+//       paymentInfo: encodeURIComponent(JSON.stringify(paymentInfo)),
+//       qrCodeData: qrCodeData
+//     });
+//     fetch(`http://localhost:3333/payments/:${orderId}`, {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+//       body: body
+//     })
+//     .then(response => {
+//       if (response.ok) {
+//         alert('Payment successful!');
+//       } else {
+//         alert('Payment failed. Please try again later.');
+//       }
+//     })
+//     .catch(error => {
+//       console.error(error);
+//       alert('Payment failed. Please try again later.');
+//     });
+//   }
